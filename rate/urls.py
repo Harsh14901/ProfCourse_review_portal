ee0b6a69e6c9b78977c689c30b78a5b87a543959
Harsh Agrawal <harsh.ag14901@gmail.com>
2020-03-19 23:50:35 +0530

    added listview and detailview

--- rate/urls.py
+++ rate/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from django.conf.urls import url
 from rate.views import *
+# import rate
 
 urlpatterns = [
+    # url(r'^profs/(?P<prof_id>[0-9]+)/$', ProfPage, name="p_data"),
+    url(r'^profs/(?P<pk>[0-9]+)/$', ProfDetailView.as_view(), name="p_data"),
     url(r'^profs/',ProfsListView.as_view(),name="profs"),
-    url(r'^courses/',CoursesListView.as_view(),name="courses"),
+    
+    # url(r'^courses/(?P<course_id>[0-9]+)/$',CoursePage,name="c_data"),
+    url(r'^courses/(?P<pk>[0-9]+)/$',CourseDetailView.as_view(),name="c_data"),
+    url(r'^courses/', CoursesListView.as_view(), name="courses"),
+    
 ]
--- rate/views.py
+++ rate/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.generic import ListView
 from django.views.generic import DetailView
 from rate.models import *
@@ -15,6 +16,41 @@ class CoursesListView(ListView):
     context_object_name = "dept_list"
 
 
+class ProfDetailView(DetailView):
+    model=Profs
+    
+    # def get_queryset(self):
+    #     q1 = super().get_queryset()
+    #     print(q1)
+        
+    #     return q1.filter(pk=int(self.kwargs['pk']))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print(context)
+        return context
+    
+    
+    
+    template_name = "prof/prof_page.html"
+    context_object_name = "prof_data"
 
 
+class CourseDetailView(DetailView):
+    model=Courses
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+    
+
+    template_name = "course/course_page.html"
+    context_object_name = "course_data"
+
+# def ProfPage(request,prof_id):
+#     return render(request=request,template_name="prof/prof_page.html",context={"prof_data":Profs.objects.get(id=prof_id)})
+    
+# def CoursePage(request,course_id):
+#     print("Course Page called")
+#     return render(request=request,template_name="course/course_page.html",context={"course_data":Courses.objects.get(id=course_id)})
     
